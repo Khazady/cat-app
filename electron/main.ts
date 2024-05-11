72c0a955860be9975c42e3e8fb97c4281fe63bef
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-05-11 11:29:12 +0200

    refactor main.ts: windows size & position, devTools, buttonWin url

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,16 @@
 import path, { join } from "path";
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow, ipcMain, screen } from "electron";
 
 const windows: BrowserWindow[] = [];
 
 function createWindows() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
   const mainWin = new BrowserWindow({
+    width: width / 2,
+    height: height,
+    x: 0,
+    y: 0,
     webPreferences: {
       preload: join(__dirname, "preload.js"),
       sandbox: true,
@@ -15,14 +21,17 @@ function createWindows() {
     mainWin.loadURL(
       path.posix.join(process.env.VITE_DEV_SERVER_URL, "html/index.html"),
     );
+    mainWin.webContents.openDevTools({ mode: "bottom" });
   } else {
     mainWin.loadFile(path.join(__dirname, "../dist/html/index.html"));
   }
   windows.push(mainWin);
 
   const buttonWin = new BrowserWindow({
-    width: 600,
-    height: 400,
+    width: width / 2,
+    height: height,
+    x: width / 2,
+    y: 0,
     webPreferences: {
       preload: join(__dirname, "preload.js"),
       sandbox: true,
@@ -30,7 +39,10 @@ function createWindows() {
     },
   });
   if (process.env.VITE_DEV_SERVER_URL) {
-    buttonWin.loadURL("http://localhost:5175/html/button.html");
+    buttonWin.loadURL(
+      path.posix.join(process.env.VITE_DEV_SERVER_URL, "html/button.html"),
+    );
+    buttonWin.webContents.openDevTools({ mode: "bottom" });
   } else {
     buttonWin.loadFile(path.join(__dirname, "../dist/html/button.html"));
   }
